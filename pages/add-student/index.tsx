e6d2714d50f046a042b6791cbeb32ce609499b23
donalawa <awadonalcien12@gmail.com>
2022-08-28 01:26:45 +0100

    setup

---
+++ pages/add-student/index.tsx
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+const addNewStudent = () => {
+    return (
+        <div>
+            <h1>Add Student</h1>
+        </div>
+    )
+}
+
+
+export default addNewStudent;
